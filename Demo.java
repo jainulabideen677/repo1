320bb7aad400b82ac37bb63f191d1d949f7030e6
jainulabideen677 <jainul.abideen677@gmail.com>
2025-08-21 05:09:19 +0530

    Commited Demo class

---
+++ Demo.java
@@ -0,0 +1,51 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Demo {
+    static class Solution {
+        private  
+        int fun(int a) {
+            return a;
+        }
+    
+    }
+    private  int fun1(int a) {
+        return a;
+    } 
+    public static void main(String[] args) {Scanner sc = new Scanner(System.in);
+        int n; 
+        n=Integer.parseInt(sc.nextLine());
+        int k=Integer.parseInt(sc.nextLine());
+                int[] arr =new int[n];   
+                for(int i=0;i<n;i++){
+                    arr[i]=Integer.parseInt(sc.nextLine());
+                }
+                int c=1;
+                List<Integer> list = new ArrayList<>();
+                for(int i=1;i<n;i++){
+                    if(arr[i]==arr[i-1])c++;
+                    else{
+                        list.add(c);c=1;
+                    }
+                }
+                list.add(c);
+                n=list.size();
+                for(int i=1;i<n;i++)
+                {
+                    list.set(i,list.get(i)+list.get(i-1));
+                }
+                int ans=list.get(k-1);
+                for(int i=k;i<n;i++)
+                {
+                    ans=Math.max(ans,list.get(i)-list.get(i-k));
+                }
+                System.out.println("ans= "+ans);
+
+    
+    }}
+      
